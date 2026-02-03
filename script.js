eca86f6030eb51d96dd975639eccfd1f979149fe
22033cc <22033cc@hvhs.school.nz>
2026-02-04 12:25:39 +1300

    figuring out the basics

--- script.js
+++ script.js
@@ -1,2 +1,6 @@
-var messageSpace = getElementbyId("welcomeMessage");
-messageSpace.innerHTML = "You've connected to the JavaScript!";
+console.log("Hello world")
+document.getElementById("WelcomeMessage").innerHTML = "you've connected!"
+
+function buttonchange(){
+    console.log("yay, you clicked the button!")
+}
